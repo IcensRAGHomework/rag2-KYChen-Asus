217ebc4240e8976aaefc2cb3bed72d9bd3912146
yuan1_chen <Yuan1_Chen@asus.com>
2025-02-12 16:32:41 +0800

    hw

--- student_assignment.py
+++ student_assignment.py
@@ -7,7 +7,11 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    docs = loader.load()
+    return docs[len(docs) - 1]
 
 def hw02_2(q2_pdf):
     pass
+
+print(hw02_1(q1_pdf))
